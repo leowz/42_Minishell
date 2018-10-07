bd8d907f0ffb5e0861071e01292cb05313c0d659
Zhang WENG <zweng@e3r4p7.42.fr>
2018-10-07 17:02:27 +0200

    remove / leaks from basename

--- srcs/ms_basename.c
+++ srcs/ms_basename.c
@@ -6,7 +6,7 @@
 /*   By: zweng <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/09 11:19:34 by zweng             #+#    #+#             */
-/*   Updated: 2018/09/09 11:19:40 by zweng            ###   ########.fr       */
+/*   Updated: 2018/10/07 17:00:53 by zweng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ms_basename(const char *filename)
 
 	if (!filename)
 		return (NULL);
+	if (ft_strcmp(filename, "/") == 0)
+		return ((char *)filename);
 	ptr = ft_strrchr(filename, '/');
 	return (ptr ? ptr + 1 : (char *)filename);
 }
